04a85362fcad5652a337ac51442fedf6e84948fe
Ing. Orlando Hernandez Cruz (OrlandoHC/Masterk3y) <orlando.hdzcruz@gmail.com>
2015-05-18 02:15:27 -0500

    Update c0co.sh
    Agregadas las funciones 3 y 4, 6 y 7, ademas del reacomodo... Desarrollo de Script Anidado para event.d

--- c0c0.sh
+++ c0c0.sh
@@ -6,16 +6,18 @@ do
     cat<<EOF
     ==============================
             Nokia N-900
-   CONSOLE CONTROL REMOTE v0.1.0-beta
+  CONSOLE CONTROL REMOTE v0.1.0-beta
        BY @OrlandoHC(Masterkey) 
     ------------------------------
     Selecciona una Opción:
 
     Desbloquear             (1)
     Bloquear                (2)
-    Tomar Foto (Frontal)    (3)
-    Reiniciar               (4)
-    Apagar (Emergencia)     (5)
+    Iniciar Servidor        (3)
+    Detener Servidor        (4)
+    Tomar Foto (Frontal)    (5)
+    Reiniciar               (6)
+    Apagar (Emergencia)     (7)
     Salir                   (X)
     
     ------------------------------
@@ -24,7 +26,11 @@ EOF
     case "$REPLY" in
     "1")  dbus-send --system --type=method_call --dest=com.nokia.mce /com/nokia/mce/request com.nokia.mce.request.req_tklock_mode_change string:"unlocked" ;;
     "2")  dbus-send --system --type=method_call --dest=com.nokia.mce /com/nokia/mce/request com.nokia.mce.request.req_tklock_mode_change string:"locked" ;;
-    "3")  /bin/sleep 10 ; /usr/bin/gst-launch v4l2camsrc device=/dev/video1 num-buffers=1 \! video/x-raw-yuv,width=640,height=480  \! ffmpegcolorspace \! jpegenc \! filesink location=/home/user/MyDocs/DCIM/photo.jpg ;;
+    "3")  /etc/init.d/lighttpd start ;;
+    "4")  /etc/init.d/lighttpd stop ;;
+    "5")  /bin/sleep 10 ; /usr/bin/gst-launch v4l2camsrc device=/dev/video1 num-buffers=1 \! video/x-raw-yuv,width=640,height=480  \! ffmpegcolorspace \! jpegenc \! filesink location=/home/user/MyDocs/DCIM/photo.jpg ;;
+    "6")  reboot ;;
+    "7")  poweroff ;;
     "s")  echo "Mayus -_-"   ;;
     "X")  exit 0;; 
      * )  echo "Opcion Invalida"     ;;
